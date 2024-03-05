b5f13a7cdd6e420f8c6667cb5edf6316291ed4fc
Biswarup Mahato <107302651+biswarupmahato7@users.noreply.github.com>
2024-03-05 17:29:52 +0530

    add new file 5/3/24

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  
+
+  return (
+    <h1>Chai or React with Vite | Biswarup Mahato</h1>
+   
+  )
+}
+
+export default App
---
+++ vite-reactPw/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      Hey Biswarup
+
+      <button id='btn'>
+        <span>
+          Submit
+        </span>
+      </button>
+    </>
+  )
+}
+
+export default App
